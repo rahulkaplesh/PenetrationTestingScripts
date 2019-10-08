f636eba56fd19994713f8acc00669fbefdc42aae
RahulKaplesh <rahulkaplesh@gmail.com>
2019-10-08 22:43:32 +0530

    changed color working with python3

--- scripts/scanning/advancedscanner.py
+++ scripts/scanning/advancedscanner.py
@@ -3,14 +3,15 @@
 from socket import *
 import optparse
 from threading import *
+from termcolor import colored
 
 def connScan(tgtHost,tgtPort):
    try:
       sock = socket(AF_INET, SOCK_STREAM)
       sock.connect((tgtHost,tgtPort))
-      print '[+]%d/tcp Open' %tgtPort
+      print (colored('[+]%d/tcp Open' %tgtPort, 'green'))
    except:
-      print '[-]%d/tcp Closed' %tgtPort
+      print (colored('[-]%d/tcp Closed' %tgtPort, 'red'))
    finally:
       sock.close()
 
@@ -18,15 +19,15 @@ def portScan(tgtHost,tgtPorts):
    try:
       tgtIP = gethostbyname(tgtHost)
    except:
-      print 'Cant Resolve the Target Host %s' %tgtHost
+      print (colored('Cant Resolve the Target Host %s' %tgtHost, 'red'))
    try:
       tgtName = gethostbyaddr(tgtIP)
-      print '[+] Scan results for:' + tgtName[0]
+      print (colored('[+] Scan results for:' + tgtName[0], 'green'))
    except:
-      print '[+] Scan results for:' + tgtIP
+      print (colored('[+] Scan results for:' + tgtIP, 'green'))
    setdefaulttimeout(1)
    for tgtPort in tgtPorts :
-      t = Thread(target=connScan, args=(tgtHost,int(tgtPort)))
+      t = Thread(target=connScan, args=(tgtHost,int(t gtPort)))
       t.start()
 
 def main():
@@ -45,7 +46,7 @@ def main():
    tgtHost = options.tgtHost
    tgtPorts = str(options.tgtPorts).split(',')
    if(tgtHost == None) | (tgtPorts[0] == None):
-      print parser.usage
+      print (parser.usage)
       exit(0)
    portScan(tgtHost,tgtPorts)
 
